1bdc6d77eef477bef1b932a3990aed8105b29945
Jonathan Sias <jcsias@inf.ufpel.edu.br>
2018-05-16 16:37:27 -0300

    Add um monte de funções

--- test.hs
+++ test.hs
@@ -1,8 +1,10 @@
 -- :l init.hs
+import Control.Monad (forM_)
+import Control.Monad (mapM_)
 
 separaLinha::String->[String]
-separaLinha " "=[]  --esse a faz ser uma string qlqr
-separaLinha a = takeWhile(> '\n') a : []
+separaLinha " "= []  --esse a faz ser uma string qlqr
+separaLinha a = dropWhile(< '\n') a : []
 
 
 tamanhoLinha::[String]->Int
@@ -13,9 +15,33 @@ tamanhoLinha ((a:x):xs)
 
 --separaPalavras :: String -> [String] a mesma coisa que word String -> [String]
 --lenght(words(string)) = n de palavras na linha
-teste::String->String
-teste a = putStr(words (a))
 
--- recebe uma string e o tamanhoMaiorLinha, conta qual o tamanho da propria - maior e add espaços entre cada palavra 
+--putStr imprime
+-- recebe uma string e o tamanhoMaiorLinha, conta qual o tamanho da propria - maior e add espaços entre cada palavra
 -- separaLinha, lenght(words(separaLinha))-1, valores pra divisao tamanhoML - tamanhoLinha = n de espaços. n de espacos / lenght-1 = X de espaços
 -- reescrever a cada espaço que achar, add o X de espaços
+
+--teste::[String] -> String
+--teste [] = " "
+--teste ((a):xs) = putStr (a)
+
+tt:: [String] -> String
+tt [] = ""
+tt ((a):xs) = foldr (++) [] [a] ++" "++ tt xs
+
+tu:: [String] -> IO()
+tu [] = return()
+tu ((a):xs) = do {putStr a;adicionaPeca 3 " "; tu xs}
+
+adicionaPeca :: Integer -> String -> IO ()
+adicionaPeca n xs = sequence_ [putStr xs| i<- [1..n]]
+
+transformaLinhaLista:: Int->Int->Int->Int->Int->Int->Int->Int->Int->[Int]
+transformaLinhaLista a b c d e f g h i = a : b : c : d : e : f : g : h : i :[]
+
+maiorLista::[Int]->Int
+maiorLista [a] = a
+maiorLista (a:xs) = max a (maiorLista xs)
+
+listaTamanhos = transformaLinhaLista 1 2 3 4 5 6 7 8 9
+maior = maiorLista listaTamanhos
