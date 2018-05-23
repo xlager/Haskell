8971fbabb40e6a0e727c1c6a23e46f95517511ac
Jonathan Sias <jcsias@inf.ufpel.edu.br>
2018-05-23 16:50:20 -0300

    mais andamento

--- test.hs
+++ test.hs
@@ -1,18 +1,6 @@
 -- :l init.hs
 import Control.Monad (forM_)
 import Control.Monad (mapM_)
-
-separaLinha::String->[String]
-separaLinha " "= []  --esse a faz ser uma string qlqr
-separaLinha a = dropWhile(< '\n') a : []
-
-
-tamanhoLinha::[String]->Int
-tamanhoLinha [] = 0
-tamanhoLinha ((a:x):xs)
-   | a == '\n' = 0
-   | otherwise = 1 + tamanhoLinha (x:xs)
-
 --separaPalavras :: String -> [String] a mesma coisa que word String -> [String]
 --lenght(words(string)) = n de palavras na linha
 
@@ -25,23 +13,55 @@ tamanhoLinha ((a:x):xs)
 --teste [] = " "
 --teste ((a):xs) = putStr (a)
 
-tt:: [String] -> String
-tt [] = ""
-tt ((a):xs) = foldr (++) [] [a] ++" "++ tt xs
+--Separa linhas - atualmente inútil
+separaLinha::String->[String]
+separaLinha " "= []  --esse a faz ser uma string qlqr
+separaLinha a = dropWhile(< '\n') a : []
 
+--Calcula o tamanho da linha
+tamanhoLinha::String->Int
+tamanhoLinha [] = 0
+tamanhoLinha (a:xs)
+   | a == '\n' = 0
+   | otherwise = 1 + tamanhoLinha xs
+
+--Função para virar a que escreve no fim
 tu:: [String] -> IO()
 tu [] = return()
 tu ((a):xs) = do {putStr a;adicionaPeca 3 " "; tu xs}
 
-adicionaPeca :: Integer -> String -> IO ()
+
+--Adiciona a string o numero de vezes
+adicionaPeca :: Int -> String -> IO ()
 adicionaPeca n xs = sequence_ [putStr xs| i<- [1..n]]
 
-transformaLinhaLista:: Int->Int->Int->Int->Int->Int->Int->Int->Int->[Int]
-transformaLinhaLista a b c d e f g h i = a : b : c : d : e : f : g : h : i :[]
 
+--Função simples que pega o maior valor de uma lista
 maiorLista::[Int]->Int
 maiorLista [a] = a
 maiorLista (a:xs) = max a (maiorLista xs)
 
-listaTamanhos = transformaLinhaLista 1 2 3 4 5 6 7 8 9
-maior = maiorLista listaTamanhos
+--Através de recursão, Pega o tamanho de cada Linha
+listaTamanhos :: [String]->[Int]
+listaTamanhos [] = []
+listaTamanhos ((a):xs) = tamanhoLinha a : listaTamanhos xs
+
+--Pegar os espaços agora
+calculaEspacos::[String]->[Int]-> Int ->[Int]
+calculaEspacos [] size bigger = []
+calculaEspacos text [] bigger = []
+--calculaEspacos te:xt si:ze bigger = []
+
+
+--Lista com todas as linhas
+texto = [linha1,linha2,linha3,linha4,linha5,linha6,linha7,linha8,linha9]
+
+linha1 = "RUBIÃO fitava a enseada eram oito horas da manhã.\n"
+linha2 = " Quem o visse com os polegares metidos no cordão do chambre à janela de uma\n"
+linha3 = " grande casa de Botafogo cuidaria que ele admirava aquele pedaço de água\n"
+linha4 = " quieta mas em verdade vos digo que pensava em outra coisa.\n"
+linha5 = " Cotejava o passado com o presente. Que era há um ano?\n"
+linha6 = " Professor. Que é agora? Capitalista! Olha para si para as chinelas\n"
+linha7 = " (umas chinelas de Túnis que lhe deu recente amigo Cristiano Palha) para a casa\n"
+linha8 = " - para o jardim para a enseada para os morros e para o céu e tudo desde as chinelas\n"
+linha9 = " até o céu tudo entra na mesma sensação de propriedade."
