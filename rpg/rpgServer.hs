b3989c6d371ba7a6337f7f46863b93cd25bc1774
Pedro Halmenschlager <phalmenschlager@inf.ufpel.edu.br>
2018-07-22 06:25:42 -0300

    Adicionado grande parte da ~interface e adquirir itens

--- rpg/rpgServer.hs
+++ rpg/rpgServer.hs
@@ -1,25 +1,26 @@
 --meter um telnet localhost 4242
 --https://www.text-image.com/convert/pic2ascii.cgi
 --https://pixlr.com/editor/
+--threadDelay 10000000 --Delay de 10 segundos
 module Main where
 
-import Network.Socket
-import System.IO
+import Network.Socket           --Biblioteca para conexão multi-computadores
+import System.IO                --Biblioteca para comandos de entrada e saída  
 import Control.Exception
-import Control.Concurrent
+import Control.Concurrent       --Biblioteca para uso de threads
 import Control.Monad (when)
 import Control.Monad.Fix (fix)
- 
+
+-------------------------- Inicialização do socket de comunicação ---------------------------------------------
 main :: IO ()
 main = do
   sock <- socket AF_INET Stream 0
   setSocketOption sock ReuseAddr 1
-  bind sock (SockAddrInet 4250 iNADDR_ANY)
+  bind sock (SockAddrInet 4243 iNADDR_ANY)
   listen sock 2
   mainLoop sock
  
-type Msg = (Int, String)
- 
+-------------------------- Socket que fica ouvindo portas e criando threads ----------------------------------- 
 mainLoop :: Socket  -> IO ()
 mainLoop sock = do
   conn <- accept sock
@@ -27,6 +28,7 @@ mainLoop sock = do
   close sock
   mainLoop sock
 
+-------------------------- Aqui começa o "Main", onde roda o jogo ---------------------------------------------
 runConn :: (Socket, SockAddr) -> IO ()
 runConn (sock,_)  = do
   hdl <- socketToHandle sock ReadWriteMode
@@ -41,55 +43,50 @@ runConn (sock,_)  = do
   hPutStrLn hdl (show roles)
   role <- setRole hdl
   hPutStrLn hdl ("Greetings to the world of Kalimarbadorna " ++ name ++ ", the " ++ role++"!")
-  character <- turnPerIO (Per (name) (role) (getRoleAtk (role)) (getRoleHp (role)) Vazio)
+  character <- turnPerIO (Per (name) (role) (getRoleAtk (role)) (getRoleHp (role)) empty)
   hPutStrLn hdl (show character)
   hPutStrLn hdl (show (getAtk character))
   hPutStrLn hdl "\nNow that you have chosen your role, lets begin our first dungeon"
-  hPutStrLn hdl "Dungeon 1\n You are in a room where you can go north, though, \
-  \you see a little goblin in front of you...\nYou will have to fight that little"
-  hPutStrLn hdl (show goblin)
-  result <- battle hdl character goblin 0
+  printMap hdl 1 
+  result <- battle hdl character dragon 0
   hPutStrLn hdl (result)
   if result == "Win"
     then do
-      hPutStrLn hdl "You got goblin's knife Atk + 1"
-      if (getEquipRole goblinKnife) == Atk
-        then do 
-          character <- turnPerIO (Per(getName character)(getRole character)(getAtk character + (getEquipAtribute goblinKnife)) (getHp character) goblinKnife)
-          hPutStrLn hdl "Congratulations"
-      else
-        do
-          character <- turnPerIO (Per(getName character) (getRole character) (getAtk character) (getHp character + (getEquipAtribute goblinKnife)) goblinKnife)
-          hPutStrLn hdl "Congratulations"
+      hPutStrLn hdl "You got an item"
+      character <- (aquireEquip hdl character goblinKnife);
+      printChar hdl character goblin
     else hPutStrLn hdl "Since you ran away, u get no item"
-  
   hClose hdl
 
 
 
--------------------------- Tipagem Algébrica ------------------------------
+-------------------------- Tipagem Algébrica ------------------------------------------------------------------
 type Nome = String
 type Atributo = Int
 
 data Personagem = Per Nome Role Atributo Atributo Equip
  deriving (Show, Eq)
-data AorH  = Atk | Hp
+data AorH  = Atk | Hp | NA
  deriving (Show,Eq)
 data Equip = Eqp Nome AorH Atributo | Vazio
  deriving (Show,Eq)
 type Role = String
--------------------------- Declaração Importantes, de Mobs e Equips --------
--------------------------- Importante --------------------------------------
+-------------------------- Declaração Importantes, de Mobs e Equips -------------------------------------------
+-------------------------- Importante -------------------------------------------------------------------------
 roles::[String]
 roles = ["Assassin","Cleric","Mage","Ranger","Warrior"]
--------------------------- Mobs --------------------------------------------
+-------------------------- Mobs -------------------------------------------------------------------------------
 goblin::Personagem
 goblin = Per "Goburin" "Monster" 1 5 Vazio
--------------------------- Equips ------------------------------------------
+dragon::Personagem
+dragon = Per "Dragon" "Monster" 100 5 Vazio
+-------------------------- Equips -----------------------------------------------------------------------------
+empty::Equip
+empty = Eqp "Bare Hands" Atk 0
 goblinKnife::Equip
 goblinKnife = Eqp "Goblin's Knife" Atk 1
 
--------------------------- Funções -----------------------------------------
+-------------------------- Funções ----------------------------------------------------------------------------
 turnPerIO::Personagem->IO Personagem
 turnPerIO (Per n r atk hp eqp) = return (Per n r atk hp eqp)
 
@@ -104,6 +101,9 @@ getName (Per n _ _ _ _) = n
 
 getRole::Personagem->String
 getRole (Per _ r _ _ _) = r
+
+getEquip::Personagem-> Equip
+getEquip (Per _ _ _ _ eqp) = eqp
 
 getRoleAtk::Role->Int
 getRoleAtk n
@@ -120,12 +120,15 @@ getRoleHp n
   | n == "Warrior"  = 5
 
 getEquipName::Equip->String
+getEquipName Vazio = "Vazio"
 getEquipName (Eqp n _ _) = n
 
 getEquipRole::Equip->AorH
+getEquipRole Vazio = NA
 getEquipRole (Eqp _ ah _) = ah
 
 getEquipAtribute::Equip->Int
+getEquipAtribute Vazio = 0
 getEquipAtribute (Eqp _ _ a) = a
 
 
@@ -284,8 +287,12 @@ roleDescription hdl role
 battle::Handle->Personagem->Personagem->Int->IO String
 battle hdl principal monstro turno
   |getHp principal <= 0  && turno /= 0 = 
-    if (show(getRole principal)) == "Cleric" 
-      then battle hdl principal monstro turno
+    if getRole principal == "Cleric" 
+      then 
+        do
+          principal <- turnPerIO  (Per(getName principal)("-Cleric")(getRoleAtk (getRole principal))(getRoleHp (getRole principal)) empty)
+          printChar hdl principal monstro
+          battle hdl principal monstro turno
       else do
         hPutStrLn hdl ("===================================================================\
         \\tVocê perdeu e terá de reiniciar o jogo!!!!!!================================\n\n\n")
@@ -297,8 +304,8 @@ battle hdl principal monstro turno
       return "Win"
   |otherwise =
     do
-      hPutStrLn hdl ("\n\n\nTurno: "++(show turno)++" entre: " ++(show (getName principal)) ++ "\
-      \e " ++ (show (getName monstro))++"!")
+      printChar hdl principal monstro
+      hPutStrLn hdl ("\n\tTurno: "++(show turno))
       hPutStrLn hdl ("\n O que deseja fazer? (Attack(a),Skill(s),Run(r))")
       action<- fmap init (hGetLine hdl)
       case action of
@@ -329,30 +336,47 @@ battle hdl principal monstro turno
             hPutStrLn hdl "You ran away from battle chicken, also, the monster will still be there"
             return "Run"
     
+printChar::Handle->Personagem->Personagem->IO ()
+printChar hdl personagem monstro =
+  do
+    hPutStrLn hdl ("\t| "++ show(getName personagem) ++"\t\t |\t\t\t| "++ show(getName monstro)++ "\t |\n\t| \
+    \Health: " ++ show(getHp personagem) ++"\t\t |\t\t\t| Health: "++ show(getHp monstro) ++ "\t |\n\t| \
+    \Attack: " ++ show(getAtk personagem) ++"\t\t |\t\t\t| Attack: "++ show(getAtk monstro) ++ "\t \n\t| \
+    \Equip: " ++ show(getEquipName (getEquip personagem)) ++"|\t\t\t| Equip: "++ show(getEquipName (getEquip monstro)) ++ "\t |")
+    
+printMap::Handle->Int->IO ()
+printMap hdl num = 
+  do
+    case num of 
+      1 -> 
+        do
+          hPutStrLn hdl ("\tYou find yourself in a room, where the only thing you can do is north, though there is\
+          \ a goblin blocking the door, you will have to fight him")
+          hPutStrLn hdl ("\t------| |------\n\
+                         \\t|      #      |\n\
+                         \\t|             |\n\
+                         \\t|      *      |\n\
+                         \\t|             |\n\
+                         \\t---------------")          
+      2 -> 
+        do
+          hPutStrLn hdl ("bye")
+      3 -> 
+        do
+          hPutStrLn hdl ("bye")
+      _ -> 
+        do
+          hPutStrLn hdl ("bye")
 
-      {--
-      if action == "a"
-        then
-          do
-            principal <- turnPerIO  (Per(getName principal)(getRole principal)(getAtk principal)((getHp principal)-(getAtk monstro)) Vazio)
-            monstro <- turnPerIO    (Per(getName monstro) (getRole monstro) (getAtk monstro) ((getHp monstro) - (getAtk principal)) Vazio)
-            battle hdl principal monstro (turno+1)
-        else
-          if action == "s"
-            then 
-              if (show(getRole principal) == "Mage")
-                then 
-                  principal <- turnPerIO  (Per(getName principal)(getRole principal)(getAtk principal)((getHp principal)-(getAtk monstro)) Vazio)
-                  monstro <- turnPerIO    (Per(getName monstro) (getRole monstro) (getAtk monstro) ((getHp monstro) - (9)) Vazio)
-                  battle hdl principal monstro (turno+1)
-                else
-                  if ((show(getRole principal))=="Warrior")
-                    then
-                      principal <- turnPerIO  (Per(getName principal)(getRole principal)(getAtk principal)((getHp principal)+3) Vazio)
-                      monstro <- turnPerIO    (Per(getName monstro) (getRole monstro) (getAtk monstro) ((getHp monstro) Vazio)
-                      battle hdl principal monstro (turno+1)
-                    else
-                      hPutStrLn hdl "You can't do that"
-                      battle hdl principal monstro turno
-            else if action == 
---}
+aquireEquip::Handle->Personagem->Equip->IO Personagem
+aquireEquip hdl character eqp = 
+  do
+    hPutStrLn hdl ("\tDo you want the: " ++ show(getEquipName eqp) ++"\t\n"++ show(getEquipRole eqp) ++ ": +" ++ show(getEquipAtribute eqp)++"(y/n)")
+    choice <- fmap init (hGetLine hdl)
+    if choice == "y" || choice == "yes"
+      then
+        case (getEquipRole eqp) of
+          Atk -> turnPerIO (Per(getName character)(getRole character)(getRoleAtk (getRole character) + (getEquipAtribute eqp)) (getHp character) eqp)
+          _ ->  turnPerIO (Per(getName character) (getRole character) (getAtk character) (getRoleHp (getRole character)  + (getEquipAtribute eqp)) eqp)
+      else
+        turnPerIO (Per(getName character) (getRole character) (getAtk character) (getHp character) (getEquip character))
